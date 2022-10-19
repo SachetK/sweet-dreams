93da18ff87fd5767427c4eb101a97507b00637c7
MondayLXJ <96320845+MondayLXJ@users.noreply.github.com>
2022-10-19 09:13:13 -0400

    attempted to make heading banners

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,11 +1,25 @@
 import type { NextPage } from 'next'
+import HeadComponent from '../components/HeadComponent'
+import ButtonComponent from '../components/ButtonComponent'
 
 const Welcome: NextPage = () => {
   return (
-    <main>
-      <h1>Welcome to the world!</h1>
-      <p>{`Jessie's world`}</p>
-    </main>
+    <>
+      <HeadComponent
+        title="Sweet Dreams - Welcome"
+        description="Welcome Page for Sweet Dreams App"
+      />
+
+      <main>
+        <div className="flex h-screen flex-col items-center justify-center space-y-96">
+          <h1>Welcome</h1>
+          <ButtonComponent text="Save & Submit" />
+        </div>
+
+        <h1>Welcome to the world!</h1>
+        <p>{`Jessie's world`}</p>
+      </main>
+    </>
   )
 }
 
