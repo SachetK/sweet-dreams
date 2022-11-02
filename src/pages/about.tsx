0277d9add2a37f0c4916692d165d14e4a240c542
MondayLXJ <96320845+MondayLXJ@users.noreply.github.com>
2022-11-01 20:23:28 -0400

    finished bios for about page. start on help & contact section

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,54 +10,94 @@ import Anthony from '../../public/profile/Anthony.jpg'
 const About: NextPage = () => {
   return (
     <>
-      <main className="h-screen overflow-x-hidden bg-main">
-        <NavigationBar />
-        <div className="ml-52 mt-11">
-          <HeadComponent
-            title="Sweet Dreams - About"
-            description="About Page for Sweet Dreams App"
-          />
-          <HeadingBanner title="About the Creators" />
-          <div className="mt-16 flex w-48 flex-col justify-center space-y-11">
-            <div className="flex flex-row gap-x-8">
-              <div>
-                <Image
-                  src={JiaXi}
-                  alt="JiaXi"
-                  height={300}
-                  width={225}
-                  priority={true}
-                />
-              </div>
-              <p className="gap-x-8">hi</p>
-            </div>
-            <div className="flex flex-row">
-              <div>
-                <Image
-                  src={Sachet}
-                  alt="Sachet"
-                  height={300}
-                  width={225}
-                  priority={true}
-                />
-              </div>
-              <p>hi</p>
-            </div>
-            <div className="flex flex-row">
-              <div>
-                <Image
-                  src={Anthony}
-                  alt="Anthony"
-                  height={300}
-                  width={225}
-                  priority={true}
-                />
-              </div>
-              <p>hi</p>
-            </div>
+  <main className="h-screen overflow-x-hidden bg-main">
+    <NavigationBar />
+    {/* about the creators part */}
+<div className="space-y-24">
+    <div className="ml-52 mt-11">
+      <HeadComponent
+        title="Sweet Dreams - About"
+        description="About Page for Sweet Dreams App"
+      />
+      <HeadingBanner 
+      title="About the Creators" 
+      />
+      <div className="flex w-full flex-col justify-center mt-16 space-y-11">
+        <div className="flex flex-row space-x-8 text-lg"> 
+          <div className="flex-none">
+           <Image
+            src={JiaXi}
+            alt="JiaXi"
+            height={300}
+            width={225}
+            priority={true}
+            />
           </div>
+            <p>Jia Xi Lin is a junior at Northwest High School.  
+              During the development of the app, she was the lead designer.  
+              Outside of the project, she enjoys eating weet fruits and listening to music.</p>
         </div>
-      </main>
+        <div className="flex flex-row space-x-8 text-lg"> 
+          <div className="flex-none">
+           <Image
+            src={Sachet}
+            alt="Sachet"
+            height={300}
+            width={225}
+            priority={true}
+            />
+          </div>
+            <p>Sachet Korada is a junior at Poolesville High School. 
+               While working on the project, he was the lead programmer. 
+               Outside of the project, he enjoys to taekwondo and play video games.
+            </p>
+        </div>
+        <div className="flex flex-row space-x-8 text-lg"> 
+          <div className="flex-none">
+           <Image
+            src={Anthony}
+            alt="Anthony"
+            height={300}
+            width={225}
+            priority={true}
+            />
+          </div>
+            <p>Anthony Wang is a junior at Northwest High School.
+               Throughout the project, he was a main designer of the user interface and researcher.
+               During his free time, Anthony likes to play rhythm games and practice archery.
+            </p>
+        </div>
+        <div className="flex flex-row space-x-8 text-lg"> 
+          <div className="flex-none">
+           <Image
+            src={Anthony}
+            alt="Anthony"
+            height={300}
+            width={225}
+            priority={true}
+            />
+          </div>
+            <p>Sakash Khanna is a junior at Northwest High School. 
+               During the development of the project, he was both a programmer and reseracher. 
+               Outside of meeting times, he likes playing chess, biking, and playing badminton.  
+            </p>
+        </div>
+    </div>
+    </div>
+
+    {/* help and contact part  */}
+
+    <div className="ml-52 mt-11">
+      <HeadingBanner 
+      title="Help & Contact" 
+      />
+      <div className="flex w-full flex-col justify-center mt-16 space-y-11">
+   
+   
+    </div>
+    </div>
+</div>
+    </main>
     </>
   )
 }
