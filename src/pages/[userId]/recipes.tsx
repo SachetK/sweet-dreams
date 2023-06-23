cfcdefe1f2207157af339da550026fc2e413095c
SachetK <71898487+SachetK@users.noreply.github.com>
2023-06-22 22:16:24 -0400

    Add Head to saved recipes page

--- src/pages/[userId]/recipes.tsx
+++ src/pages/[userId]/recipes.tsx
@@ -2,19 +2,29 @@ import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import RecipeComponent from "../../components/RecipeComponent";
 import { api } from "~/utils/api";
+import HeadComponent from "~/components/HeadComponent";
 
 const SavedRecipes: NextPage = () => {
-  const { data: session } = useSession();
+  const { data: session } = useSession({
+    required: true,
+  });
+
   const { data: user } = api.user.byId.useQuery(
     { id: session?.user?.id ?? "" },
     { enabled: !!session }
   );
+
   const { data: recipes } = api.recipe.byIds.useQuery(
     { ids: user?.savedRecipes ?? [] },
     { enabled: !!user }
   );
+
   return (
     <main className="h-screen bg-main">
+      <HeadComponent
+        title={`${user?.name ?? "Sweet Dreams"} - Saved Recipes`}
+        description={`Saved recipes page for ${user?.name ?? "user"}`}
+      />
       <h1 className="text-bold text-center text-6xl">Saved Recipes</h1>
       <div className="mx-[32%] my-4 w-max">
         {recipes?.map((recipe) => (
--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,11 +1,25 @@
 import type { NextPage } from "next";
 import { useHistory } from "../components/HistoryProvider";
 import RecipeComponent from "../components/RecipeComponent";
+import { useSession } from "next-auth/react";
+import HeadComponent from "~/components/HeadComponent";
 
 const History: NextPage = () => {
   const { history } = useHistory();
+  const { data: session, status } = useSession({
+    required: true,
+  });
+
+  if (status === "loading") {
+    return <></>;
+  }
+
   return (
     <main className="h-screen bg-main">
+      <HeadComponent
+        title={`${session.user.name ?? "Sweet Dreams"} - Saved Recipes`}
+        description={`Saved recipes page for ${session.user.name ?? "user"}`}
+      />
       <h1 className="text-bold text-center text-6xl">History</h1>
       <div className="mx-[32%] my-4 w-max">
         {history.reverse().map((recipe) => (
